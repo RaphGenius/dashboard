36e3dcbfb1fba1a716db483e7fafe22a009bb24f
Raphael <guet.raphael@hotmail.com>
2023-01-11 16:06:07 +0100

    Finalisation du tableau budget depense, avec tooltip

--- src/components/Charts/Stacked.jsx
+++ src/components/Charts/Stacked.jsx
@@ -11,7 +11,7 @@ import {
 } from "@syncfusion/ej2-react-charts/src";
 
 import {
-  stackedCustimSeries,
+  stackedCustomSeries,
   stackedPrimaryXAxis,
   stackedPrimaryYAxis,
 } from "../../data/dummy";
@@ -21,11 +21,19 @@ const Stacked = ({ width, height }) => {
     <ChartComponent
       width={width}
       height={height}
-      id="stack chart"
+      id="charts"
       primaryXAxis={stackedPrimaryXAxis}
       primaryYAxis={stackedPrimaryYAxis}
+      chartArea={{ border: { width: 0 } }}
+      tooltip={{ enable: true }}
+      legendSettings={{ background: "white" }}
     >
       <Inject services={[Legend, Category, StackingColumnSeries, Tooltip]} />
+      <SeriesCollectionDirective>
+        {stackedCustomSeries.map((item, index) => (
+          <SeriesDirective key={index} {...item} />
+        ))}
+      </SeriesCollectionDirective>
     </ChartComponent>
   );
 };
